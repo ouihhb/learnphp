bc26c46c707f372d80d6c9ab983e92a8787d2066
Maksim Kalinski <makskrik07@gmail.com>
2026-09-16 11:45:24 +0300

    Add vidibility example

--- index.php
+++ index.php
@@ -2,24 +2,25 @@
 
 class Box {
    public $width;
-   public $height;
-   public $lenght;
+   private $height;
+   protected $lenght;
+
 
    public function volume(){
     return $this->width * $this->height * $this->lenght;
    }
 }
 
-$num1 = 1;
-$num2 = &$num1;
-$num1 = 2;
- var_dump($num1, $num2);
+class MetalBox extends Box {
+   public $weightPerUnit;
+   public function mass(){
+    return $this->volume() * $this->weightPerUnit;
+   }
+}
 
- $box1 = new Box();
- $box1->width = 1;
- $box2 = clone $box1;
- $box2->width = $box1->width;
- $box1->width = 2;
- 
- var_dump($box1->width, $box2->width);
- 
+$box1 =new Box();
+$box1->height = 10;
+var_dump($box1->lenght);
+
+$metal1 = new MetalBox();
+var_dump($metal1);
