373366173b044b3067a16e69b3df1879b7bde8aa
Maksim Kalinski <makskrik07@gmail.com>
2026-09-18 13:00:51 +0300

    add traits examples

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 class Box {
+   use HasColor;
    public $width;
    private $height;
    protected $lenght;
@@ -24,3 +25,10 @@ var_dump($box1->lenght);
 
 $metal1 = new MetalBox();
 var_dump($metal1);
+
+trait HasColor {
+   public $color;
+   public function setColor($color) [
+      $this->color = $color;
+   ]
+}
